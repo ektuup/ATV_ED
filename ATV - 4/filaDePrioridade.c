6fc8c53c2784f8026626347f4a13a5a3f7868d2a
Glaucio <glauciovictor120@gmail.com>
2026-04-07 07:19:50 -0300

    adicionando leitura em arquivo .txt

--- ATV - 4/filaDePrioridade.c
+++ ATV - 4/filaDePrioridade.c
@@ -6,49 +6,43 @@
 #define or ||
 #define not !
 #define mod %
+#define MAX_PESSOAS 128
+#define PESSOA_INVALIDA (Pessoa){(String){""}, -1}
 
-typedef unsigned char uint8;
 typedef enum{NORMAL, HIGH}Priority;
 
-typedef struct{
-    char data[64];
-    int len;
-}String;
-
-typedef struct Pessoa{
-    String nome;
-    uint8 idade;
-}Pessoa;
-
-Priority IdadeToPriority(uint8 idade){
+Priority IdadeToPriority(int idade){
     return idade < 60 ? NORMAL : HIGH;
 }
 
-Pessoa new_Pessoa(String nome, uint8 idade){
-    return (Pessoa){nome, idade};
-}
+typedef struct{
+    char data[64];
+}String;
 
 String new_String(const char* str){
     String S;
     if(str == NULL){
-        S.data[0] = '\0';
-        S.len = 0;
+        S.data[0] = '\0'; //caso str seja um ponteiro nulo, a string é vazia;
         return S;
     }
     strcpy(S.data, str);
-    S.len = strlen(str);
     return S;
+}
+
+typedef struct Pessoa{
+    String nome;
+    int idade;
+}Pessoa;
+
+//construtor de Pessoa
+Pessoa new_Pessoa(String nome, int idade){ 
+    return (Pessoa){nome, idade};
 }
 
 typedef struct node{
     Pessoa data;
     struct node* next;
 }node;
-
-typedef struct PriorityQueue{
-    node* highPriority, *normalPriority;
-    int contDequeue;
-}PriorityQueue;
 
 node* new_node(Pessoa p){
     node* new = malloc(sizeof(node));
@@ -57,11 +51,18 @@ node* new_node(Pessoa p){
     return new;
 }
 
+typedef struct PriorityQueue{
+    node* highPriority, *normalPriority;
+    int contDequeue;
+    size_t size;
+}PriorityQueue;
+
 PriorityQueue* new_priorityQueue(){
     PriorityQueue* pq = malloc(sizeof(PriorityQueue));
     pq->highPriority = NULL;
     pq->normalPriority = NULL;
     pq->contDequeue = 0;
+    pq->size = 0;
     return pq;
 }
 
@@ -73,11 +74,11 @@ void enQueue(PriorityQueue* pq, Pessoa p){
         *aux = new;
     }else{
         node* temp = *aux;
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-        temp->next = new;
+        while((temp)->next != NULL)
+            (temp) = (temp)->next;
+        (temp)->next = new;
     }
+    pq->size++;
     printf("%s inserido na lista de prioridade %s\n", p.nome.data, (pr == HIGH) ? "Alta" : "Normal");
 }
 
@@ -89,6 +90,7 @@ int IsEmpty(PriorityQueue* pq, Priority p){
 Pessoa deQueue(PriorityQueue* pq){
     //tira da fila normal enquant cont < 3 e a fila normal nao ser vazia.
     //se cont == 3 ou a fila normal estar vazia, tira da de prioridade alta.
+    if(pq->size == 0) return PESSOA_INVALIDA;
     node* aux = NULL;
     if(pq->contDequeue == 0 and not IsEmpty(pq, HIGH)){
         aux = pq->highPriority;
@@ -103,6 +105,7 @@ Pessoa deQueue(PriorityQueue* pq){
         pq->highPriority = pq->highPriority->next;
         pq->contDequeue = (pq->contDequeue + 1) mod 4;
     }
+    pq->size--;
     Pessoa p = aux->data;
     free(aux);
     return p;
@@ -145,26 +148,52 @@ void print_priorityQueue(PriorityQueue* pq){
     putchar('\n');
 }
 
+int ReadFile(const char* filename, Pessoa* p){
+    FILE* arq = fopen(filename, "r");
+    String line = new_String(""), nome;
+    int idade;
+    int i = 0, cont = 0, olha;
+    while(fgets(line.data, sizeof(line.data), arq)){
+        while(line.data[i] == ' ') i++;
+        if(line.data[i] == '\0') continue;
+        
+        int lido = sscanf(line.data, "%63[^;];%d", nome.data, &idade);
+
+        if(lido < 2){
+            line.data[strlen(line.data) - 1] = '\0';
+            printf("Erro em '%s' : cada linha do arquivo deve ser do formato 'nome;idade'\n", line.data);
+            exit(EXIT_FAILURE);
+        }
+        if(cont == MAX_PESSOAS){
+            printf("Erro: capacidade maxima de pessoas permitido é %d. por favor aumente a capacidade caso queira armazenar mais\n", MAX_PESSOAS);
+            return cont;
+        }
+
+        p[cont++] = new_Pessoa(nome, idade);
+    }
+    return cont;
+}
+
 int main(){
     PriorityQueue* fila = new_priorityQueue();
-    const char* nomes[] = {"Ana", "Glaucio", "Artur", "Heytor", "Karina", "Eliza", "Antonia", "Luiza", "Marina", "Jose"};
-    uint8 idades[] = {18, 20, 19, 21, 45, 6, 60, 65, 62, 84};
-    int n = sizeof(nomes)/sizeof(nomes[0]);
-    Pessoa pessoas[n];
-    Pessoa atendida;
-    int cont_atendidas = 0;
+    const char* filename = "clientes.txt";
+    Pessoa pessoas[MAX_PESSOAS], atendida;
+    int n = ReadFile(filename, pessoas);
+    int cont_atendidas = 0; 
     for(int i = 0; i < n; i++){
-        pessoas[i] = new_Pessoa(new_String(nomes[i]), idades[i]);
         enQueue(fila, pessoas[i]);
+        printf("%s;%d\n", pessoas[i].nome.data, pessoas[i].idade);
     }
     print_priorityQueue(fila);
 
     for(int i = 0; i < n; i++){
-        atendida = deQueue(fila);
-        Priority p = IdadeToPriority(atendida.idade);
-        printf("%s foi atendido(a) na fila com prioridade %s\n", atendida.nome.data, (p == HIGH) ? "Alta" : "Normal");
+        if(fila->size){
+            atendida = deQueue(fila);
+            Priority p = IdadeToPriority(atendida.idade);
+            cont_atendidas++;
+            printf("%s foi atendido(a) na fila com prioridade %s\n", atendida.nome.data, (p == HIGH) ? "Alta" : "Normal");
+        }
     }
-
     delete_priorityQueue(&fila);
     return 0;
 }
